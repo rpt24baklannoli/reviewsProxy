10424a3da177c119034dede170c521f983cf1af9
Tina-Luk <tina.luk.tina@gmail.com>
2020-12-03 15:42:08 -0800

    Able to render 2 services

--- client/index.js
+++ client/index.js
@@ -1,12 +1,18 @@
 let fetsyReviewsURL = document.getElementById('fetsy-reviews');
+let fetsySellerURL = document.getElementById('fetsy-seller');
 let itemID = window.location.href.slice(-2, -1);
 let reviewsID = fetsyReviewsURL.src.slice(-1);
+let sellerID = fetsySellerURL.src.slice(-1);
 const updateSrc = function (port) {
-	fetsyReviewsURL.src = `http://localhost:${port}/items/${itemID}`;
+	if (port === 3002) {
+		fetsyReviewsURL.src = `http://localhost:${port}/items/${itemID}`;
+	}
+	if (port === 3005) {
+		fetsySellerURL.src = `http://localhost:${port}/items/${itemID}`;
+	}
 };
 
-if (itemID !== reviewsID) {
+if (itemID !== reviewsID || itemID !== sellerID) {
 	updateSrc(3002);
+	updateSrc(3005);
 }
-
-console.log(itemID, reviewsID, fetsyReviewsURL);
